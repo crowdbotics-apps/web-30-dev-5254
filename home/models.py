730814db8afd0231225cc2b4658da74c4a9dcd16
Crowdbotics <team@crowdbotics.com>
2020-05-30 05:57:28 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -8,6 +8,12 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    gggfgf = models.BigIntegerField(null=True, blank=True,)
+    kjuyiu = models.BigIntegerField(null=True, blank=True,)
+    uyiuyi = models.BigIntegerField(null=True, blank=True,)
+    jhgjh = models.BigIntegerField(null=True, blank=True,)
+    jhgj = models.BigIntegerField(null=True, blank=True,)
+    nbhjg = models.BigIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
